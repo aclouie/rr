83811f11dce0baba12ff28da1490d9fcd9c5d2fa
afkbox <alouiephotography@gmail.com>
2014-07-15 15:46:24 -0500

    Setup Resource Structures
    Add make file, photos folder to use as test

---
+++ make.bat
@@ -0,0 +1,6 @@
+@echo RR Comand-Line Compile Starting
+@echo off
+C:\Windows\Microsoft.NET\Framework\v4.0.30319\csc.exe folder\folder.cs -nologo
+@echo.
+@echo RR Command-Line Compile Complete
+@echo on
